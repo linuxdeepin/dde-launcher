9260267ecb82f126ecdac9db457627c3df92df75
Hualet Wang <mr.asianwang@gmail.com>
2016-01-05 15:13:29 +0800

    update ts source
    Change-Id: I91028c54822640feb5d13b37a463e3e51789cbac

--- translations/dde-launcher.ts
+++ translations/dde-launcher.ts
@@ -4,17 +4,17 @@
 <context>
     <name>ConfirmUninstallDialog</name>
     <message>
-        <location filename="../dialogs/confirmuninstalldialog.cpp" line="11"/>
+        <location filename="../dialogs/confirmuninstalldialog.cpp" line="12"/>
         <source>All dependencies will be removed together</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dialogs/confirmuninstalldialog.cpp" line="14"/>
+        <location filename="../dialogs/confirmuninstalldialog.cpp" line="15"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dialogs/confirmuninstalldialog.cpp" line="14"/>
+        <location filename="../dialogs/confirmuninstalldialog.cpp" line="15"/>
         <source>Confirm</source>
         <translation type="unfinished"></translation>
     </message>
@@ -22,27 +22,27 @@
 <context>
     <name>DisplayModeFrame</name>
     <message>
-        <location filename="../views/displaymodeframe.cpp" line="34"/>
+        <location filename="../views/displaymodeframe.cpp" line="38"/>
         <source>By name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../views/displaymodeframe.cpp" line="36"/>
+        <location filename="../views/displaymodeframe.cpp" line="40"/>
         <source>By category (icon)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../views/displaymodeframe.cpp" line="38"/>
+        <location filename="../views/displaymodeframe.cpp" line="42"/>
         <source>By category (text)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../views/displaymodeframe.cpp" line="40"/>
+        <location filename="../views/displaymodeframe.cpp" line="44"/>
         <source>By time installed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../views/displaymodeframe.cpp" line="42"/>
+        <location filename="../views/displaymodeframe.cpp" line="46"/>
         <source>By frequency</source>
         <translation type="unfinished"></translation>
     </message>
@@ -50,114 +50,51 @@
 <context>
     <name>LauncherApp</name>
     <message>
-        <location filename="../app/launcherapp.cpp" line="36"/>
-        <source>Are you sure to uninstall %1</source>
+        <location filename="../app/launcherapp.cpp" line="47"/>
+        <source>Are you sure to uninstall %1 ?</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>MenuController</name>
     <message>
-        <location filename="../controller/menucontroller.cpp" line="63"/>
+        <location filename="../controller/menucontroller.cpp" line="70"/>
         <source>Open(_O)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../controller/menucontroller.cpp" line="67"/>
+        <location filename="../controller/menucontroller.cpp" line="74"/>
         <source>Remove from desktop</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../controller/menucontroller.cpp" line="69"/>
+        <location filename="../controller/menucontroller.cpp" line="76"/>
         <source>Send to desktop(_E)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../controller/menucontroller.cpp" line="73"/>
+        <location filename="../controller/menucontroller.cpp" line="80"/>
         <source>Remove from dock</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../controller/menucontroller.cpp" line="75"/>
+        <location filename="../controller/menucontroller.cpp" line="82"/>
         <source>Send to dock(_C)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../controller/menucontroller.cpp" line="80"/>
-        <source>remove from startup(_R)</source>
+        <location filename="../controller/menucontroller.cpp" line="87"/>
+        <source>Remove from startup(_R)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../controller/menucontroller.cpp" line="82"/>
+        <location filename="../controller/menucontroller.cpp" line="89"/>
         <source>Add to startup(_A)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../controller/menucontroller.cpp" line="84"/>
+        <location filename="../controller/menucontroller.cpp" line="91"/>
         <source>Uninstall</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../controller/menucontroller.cpp" line="345"/>
-        <source>Uninstall %1 successfully!</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>QObject</name>
-    <message>
-        <location filename="../app/global.h" line="32"/>
-        <source>internet</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../app/global.h" line="33"/>
-        <source>multimedia</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../app/global.h" line="34"/>
-        <source>games</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../app/global.h" line="35"/>
-        <source>graphics</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../app/global.h" line="36"/>
-        <source>productivity</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../app/global.h" line="37"/>
-        <source>industry</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../app/global.h" line="38"/>
-        <source>education</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../app/global.h" line="39"/>
-        <source>development</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../app/global.h" line="40"/>
-        <source>system</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../app/global.h" line="41"/>
-        <source>utilities</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../app/global.h" line="42"/>
-        <source>others</source>
         <translation type="unfinished"></translation>
     </message>
 </context>

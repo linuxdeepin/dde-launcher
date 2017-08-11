30a0d0cd7043e9c2846e204e68dd90c2c12225ff
石博文 <sbw@sbw.so>
2017-08-11 09:57:32 +0800

    fix dde-dock not shown when mini launcher show in 2d wm
    Change-Id: I71f2569ea10e7241d2b885c5bbd5376cd7ef8e08

--- miniframe.cpp
+++ miniframe.cpp
@@ -144,7 +144,7 @@ MiniFrame::MiniFrame(QWidget *parent)
     centralLayout->setContentsMargins(10, 0, 10, 0);
 
     setMaskColor(DarkColor);
-    setWindowFlags(Qt::X11BypassWindowManagerHint | Qt::WindowStaysOnTopHint);
+    setWindowFlags(Qt::FramelessWindowHint | Qt::SplashScreen | Qt::WindowStaysOnTopHint);
     setAttribute(Qt::WA_InputMethodEnabled, true);
     setFocusPolicy(Qt::ClickFocus);
     setFixedSize(550, 485);

9fdf50732276b9a9d6861f542f914cf05843696f
rekols <rekols@foxmail.com>
2018-05-15 20:28:51 +0800

    adjust rounded button
    Change-Id: I1ae2315a71ac04f0307b00902e0522c21bc93ef8

--- widgets/roundedbutton.cpp
+++ widgets/roundedbutton.cpp
@@ -24,7 +24,7 @@ RoundedButton::RoundedButton(QWidget *parent)
 {
     setObjectName("RoundedButton");
     setFocusPolicy(Qt::NoFocus);
-    setFixedWidth(75);
+    setFixedSize(60, 30);
 }
 
 RoundedButton::~RoundedButton()

ab388492b0961b595c15135767fa85a4585b5b15
niecheng <niecheng_cm@deepin.com>
2019-12-11 20:03:51 +0800

    feat(CategoryView): add blurBoxWidget LICENSE
    Change-Id: Idd0c3307f5491ecf222eb04af7d2ff1cc114d478

--- src/widgets/blurboxwidget.cpp
+++ src/widgets/blurboxwidget.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2019 ~ 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     niecheng <niejiashan@163.com>
+ *
+ * Maintainer: niecheng <niejiashan@163.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "blurboxwidget.h"
 #include "src/global_util/calculate_util.h"
 #include <QMouseEvent>
@@ -33,12 +54,12 @@ void BlurBoxWidget::layoutAddWidget(QWidget *child, int stretch, Qt::Alignment a
 
 void BlurBoxWidget::paintEvent(QPaintEvent *event)
 {
-    DBlurEffectWidget::paintEvent(event);
+   DBlurEffectWidget::paintEvent(event);
     if (m_maskLayer) {
-        QPainter painter(this);
         m_maskLayer->raise();
+        QPainter painter(this);
         painter.setRenderHint(QPainter::Antialiasing);  // 反锯齿;
-        painter.setBrush(QBrush(QColor(0, 0, 0, 100)));
+        painter.setBrush(QBrush(QColor(0, 0, 0, 67)));
         painter.setPen(Qt::transparent);
         QRect rect = this->rect();
         rect.setWidth(rect.width() - 1);

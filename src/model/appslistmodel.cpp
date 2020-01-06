b9755882412f8d14f47b3e4377c8257d7413b7f1
wangleiyong <wangleiyong_cm@deepin.com>
2020-01-06 13:39:10 +0800

    feat:Modify font size
    修改字体大小（任务：13110）
    
    Change-Id: Ib0d079e50169b3ab6fba376d1d72eb6d56e86b3c

--- src/model/appslistmodel.cpp
+++ src/model/appslistmodel.cpp
@@ -33,6 +33,7 @@
 #include <QGSettings>
 #include <DHiDPIHelper>
 #include <DGuiApplicationHelper>
+#include <DFontSizeManager>
 
 DWIDGET_USE_NAMESPACE
 DGUI_USE_NAMESPACE
@@ -321,7 +322,7 @@ QVariant AppsListModel::data(const QModelIndex &index, int role) const
     case AppIconSizeRole:
         return m_calcUtil->appIconSize();
     case AppFontSizeRole:
-        return m_calcUtil->appItemFontSize();
+        return DFontSizeManager::instance()->fontPixelSize(DFontSizeManager::T6);
     case AppItemIsDraggingRole:
         return indexDragging(index);
     case CategoryEnterIconRole:

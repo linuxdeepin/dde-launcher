b632c2bd6c3472a413fc1dbaa0a50dc12a40e763
niecheng_cm <niejiashan@163.com>
2020-03-03 13:27:15 +0800

    fix: dde-defender can be uninstalled(bug:14822)
    安全中心能被卸载
    
    Change-Id: I1eb320978f2fc61749d19050c79f6f190f8e3168

--- src/model/appslistmodel.cpp
+++ src/model/appslistmodel.cpp
@@ -73,6 +73,7 @@ const QStringList sysHoldPackages()
                << "dde-calendar"
                << "deepin-manual"
                << "deepin-terminal"
+               << "deepin-defender"
                << "kwin-wm-multitaskingview"
                << "dde-printer";
 

ae0715dc88bfa1b71138166aeb8552eafcd3cc67
lizhongming <lizhongming@uniontech.com>
2020-05-06 16:26:28 +0800

    feat: Update app uninstall prompt
    更新应用卸载弹窗的提示语(task:20257)

--- translations/dde-launcher.ts
+++ translations/dde-launcher.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
 <context>
     <name>AppsManager</name>
     <message>
@@ -62,7 +64,11 @@
     </message>
     <message>
         <source>Are you sure you want to uninstall %1 ?</source>
-        <translation>Are you sure you want to uninstall %1 ?</translation>
+        <translation type="vanished">Are you sure you want to uninstall %1 ?</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to uninstall?</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -338,7 +344,11 @@
     </message>
     <message>
         <source>Are you sure you want to uninstall %1 ?</source>
-        <translation>Are you sure you want to uninstall %1 ?</translation>
+        <translation type="vanished">Are you sure you want to uninstall %1 ?</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to uninstall?</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>

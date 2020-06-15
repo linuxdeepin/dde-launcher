203d6ec97e69d579ab1620baa82c73bade46696d
niecheng <niecheng_cm@deepin.com>
2020-06-15 11:10:49 +0800

    fix: 更换壁纸后打开启动器切换为全屏自由模式背景没有及时随壁纸更新
    由于系统系能,生成高斯模糊图片需要时间,不能及时返回,导致壁纸不能及时更新
    
    Log: 全屏模式,更换壁纸后打开启动器切换为全屏自由模式背景及时随壁纸更新(bug:31299)
    Bug: https://pms.uniontech.com/zentao/bug-view-31299.html

--- src/boxframe/backgroundmanager.cpp
+++ src/boxframe/backgroundmanager.cpp
@@ -69,7 +69,9 @@ void BackgroundManager::updateBlurBackgrounds()
     m_timerUpdateBlurbg->setInterval(1000);
     QString path = getLocalFile(m_wmInter->GetCurrentWorkspaceBackground());
     QString filePath = QFile::exists(path) ? path : DefaultWallpaper;
-    m_blurBackground = m_imageEffectInter->Get("", m_imageblur->Get(filePath));
+
+    if (m_imageblur->Get(filePath) != "")
+        m_blurBackground = m_imageEffectInter->Get("", m_imageblur->Get(filePath));
     m_background = m_imageEffectInter->Get("", filePath);
 
     if(m_blurBackground != "" && m_background != ""){

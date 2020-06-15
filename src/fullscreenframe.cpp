068d8226d40fcb4f87a374334b17efee64829fb2
niecheng <niecheng_cm@deepin.com>
2020-06-15 10:33:20 +0800

    fix: 触摸屏在全屏分类模式时划动当前分类的应用区域到下一个分类的应用区域时失败
    触摸滑动太快,导致鼠标事件被分类模块给吞噬了,造成滑动终止
    
    Log: 触摸屏在全屏分类模式时划动当前分类的应用区域到下一个分类的应用区域时失败(bug:32675)
    Bug: https://pms.uniontech.com/zentao/bug-view-32675.html

--- src/fullscreenframe.cpp
+++ src/fullscreenframe.cpp
@@ -474,7 +474,13 @@ void FullScreenFrame::hideEvent(QHideEvent *e)
 void FullScreenFrame::mousePressEvent(QMouseEvent *e)
 {
     if (e->button() != Qt::LeftButton)
+       return;
+    //鼠标在分类中间是无法滑动的
+    auto sysBoxWidgetGlobalY = m_contentFrame->mapTo(window(), m_systemBoxWidget->pos()).y();
+    if ((e->globalY() > sysBoxWidgetGlobalY && e->globalY() < (sysBoxWidgetGlobalY + m_systemBoxWidget->height())))
+    {
         return;
+    }
     m_mouse_press = true;
     m_appsAreaHScrollBarValue = m_appsArea->horizontalScrollBar()->value();
     m_mouse_press_time =  QDateTime::currentDateTime().toMSecsSinceEpoch();
@@ -484,6 +490,12 @@ void FullScreenFrame::mousePressEvent(QMouseEvent *e)
 void FullScreenFrame::mouseMoveEvent(QMouseEvent *e)
 {
     if (!m_mouse_press  || e->button() == Qt::RightButton || m_displayMode != GROUP_BY_CATEGORY) {
+        return;
+    }
+    //鼠标在分类中间是无法滑动和点击的
+    auto sysBoxWidgetGlobalY = m_contentFrame->mapTo(window(), m_systemBoxWidget->pos()).y();
+    if((e->globalY() > sysBoxWidgetGlobalY && e->globalY() < (sysBoxWidgetGlobalY + m_systemBoxWidget->height())))
+    {
         return;
     }
 
@@ -496,7 +508,8 @@ void FullScreenFrame::mouseMoveEvent(QMouseEvent *e)
 
 void FullScreenFrame::mouseReleaseEvent(QMouseEvent *e)
 {
-    if (e->button() != Qt::LeftButton)
+
+    if (e->button() != Qt::LeftButton || !m_mouse_press)
         return;
 
     if (m_mousePos == e->pos()) {

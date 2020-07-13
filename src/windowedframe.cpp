162a184fd1f1e148ffeca3b169e992d1f03434eb
xiehui <xiehui@uniontech.com>
2020-07-13 14:48:53 +0800

    fix: 修改焦点状态，让该情况保持正常
    点击所有分类后，焦点并未切换到所有分类的按钮上，而是在搜索栏
    
    Log: 启动器窗口模式，当进入分类模式时，鼠标移至返回按钮点击回车，启动器回到默认界面但仍显示返回按钮
    Task: https://pms.uniontech.com/zentao/task-view-28943.html

--- src/windowedframe.cpp
+++ src/windowedframe.cpp
@@ -878,11 +878,14 @@ void WindowedFrame::onSwitchBtnClicked()
     if (m_displayMode == All) {
         m_appsModel->setCategory(AppsListModel::Category);
         m_displayMode = Category;
+        m_focusPos = LeftBottom;
     } else if (m_displayMode == Category && m_appsModel->category() != AppsListModel::Category) {
+        m_focusPos = LeftBottom;
         m_appsModel->setCategory(AppsListModel::Category);
     } else {
         m_displayMode = All;
         m_appsModel->setCategory(AppsListModel::Custom);
+        m_focusPos = Applist;
     }
 
     m_switchBtn->updateStatus(m_displayMode);

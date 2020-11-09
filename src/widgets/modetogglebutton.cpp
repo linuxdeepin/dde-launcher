b1b6ac4a1613a7832d53035eb7cce65c86d0e918
苏义航 <suyihang@uniontech.com>
2020-11-09 11:13:23 +0800

    fix: 修复启动器切换模式按钮hover颜色不正确的问题
    模式切换按钮控件类型为DToolButton,不能修改hover颜色,新增一个继承DToolButton类,
    通过背景重绘实现hover颜色修改
    
    Log: 启动器切换模式按钮hover颜色跟改界面其他按钮保持一致
    Bug: https://pms.uniontech.com/zentao/bug-view-51928.html
    Change-Id: I34e42efcd647547d46d2c153a055b6647806757e

---
+++ src/widgets/modetogglebutton.cpp
@@ -0,0 +1,88 @@
+/*
+* Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd.
+*
+* Author:     suyihang <suyihang@uniontech.com>
+*
+* Maintainer: suyihang <suyihang@uniontech.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "modetogglebutton.h"
+
+#include <DGuiApplicationHelper>
+
+#include <QPainter>
+
+DGUI_USE_NAMESPACE
+DWIDGET_USE_NAMESPACE
+
+ModeToggleButton::ModeToggleButton(QWidget *parent)
+    : DToolButton(parent)
+{
+    setFocusPolicy(Qt::NoFocus);
+
+    resetStyle();
+    connect(DGuiApplicationHelper::instance(), &DGuiApplicationHelper::themeTypeChanged, this, [ = ] {
+        resetStyle();
+        update();
+    });
+}
+
+void ModeToggleButton::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event);
+
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing, true);
+
+    // 绘制背景
+    if (hasFocus() || m_hover) {
+        QPainterPath path;
+        path.addRoundedRect(rect(), 8, 8);
+        painter.fillPath(path, m_colorHover);
+    }
+
+    // 绘制图标
+    if (!icon().isNull()) {
+        painter.drawPixmap(rect().center().x() - iconSize().width() / 2 + 1 , rect().center().y() - iconSize().height() / 2 + 1, icon().pixmap(iconSize()));
+    }
+}
+
+void ModeToggleButton::enterEvent(QEvent *event)
+{
+    DToolButton::enterEvent(event);
+
+    m_hover = true;
+    update();
+}
+
+void ModeToggleButton::leaveEvent(QEvent *event)
+{
+    DToolButton::leaveEvent(event);
+
+    m_hover = false;
+    update();
+}
+
+void ModeToggleButton::resetStyle()
+{
+    if (DGuiApplicationHelper::DarkType == DGuiApplicationHelper::instance()->themeType()) {
+        m_colorHover.setRgb(255, 255, 255, 25);
+        setIcon(QIcon(":/icons/skin/icons/fullscreen_dark.svg"));
+    } else {
+        m_colorHover.setRgb(0, 0, 0, 25);
+        setIcon(QIcon(":/icons/skin/icons/fullscreen_normal.svg"));
+    }
+}
---
+++ src/widgets/modetogglebutton.h
@@ -0,0 +1,49 @@
+/*
+* Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd.
+*
+* Author:     suyihang <suyihang@uniontech.com>
+*
+* Maintainer: suyihang <suyihang@uniontech.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MODETOGGLEBUTTON_H
+#define MODETOGGLEBUTTON_H
+
+#include <DToolButton>
+
+DWIDGET_USE_NAMESPACE
+
+class ModeToggleButton : public DToolButton
+{
+    Q_OBJECT
+
+public:
+    ModeToggleButton(QWidget *parent = nullptr);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void enterEvent(QEvent *event) override;
+    void leaveEvent(QEvent *event) override;
+
+private:
+    void resetStyle();
+
+private:
+    bool m_hover = false;
+    QColor m_colorHover;
+};
+
+#endif

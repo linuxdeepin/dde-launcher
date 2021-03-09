b2c28cc4c6f7622bf84fc495d622f646b539f852
chengbo <chengbo@uniontech.com>
2021-03-09 16:09:30 +0800

    feat: 单元测试
    新增代码忘删除崩溃代码
    
    Log: 单元测试修改
    Change-Id: Id80ee4e04956ba0edb3c9d69eefec65c42a48c6b

--- tests/view_unit_test/tst_multipagesview.cpp
+++ tests/view_unit_test/tst_multipagesview.cpp
@@ -31,17 +31,17 @@ TEST_F(Tst_Multipagesview, multiPagesView_test)
 {
     widget->updatePageCount(AppsListModel::Category);
     QMouseEvent event(QEvent::MouseButtonPress, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier);
-    QApplication::sendEvent(widget->pageView(0)->viewport(), &event);
+    QApplication::sendEvent(widget, &event);
 
     widget->updatePageCount(AppsListModel::Internet);
     QMouseEvent event1(QEvent::MouseButtonPress, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier);
-    QApplication::sendEvent(widget->pageView(0)->viewport(), &event1);
+    QApplication::sendEvent(widget, &event1);
 
     QMouseEvent event2(QEvent::MouseMove, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier);
-    QApplication::sendEvent(widget->pageView(0)->viewport(), &event2);
+    QApplication::sendEvent(widget, &event2);
 
     QMouseEvent event3(QEvent::MouseButtonRelease, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier);
-    QApplication::sendEvent(widget->pageView(0)->viewport(), &event3);
+    QApplication::sendEvent(widget, &event3);
 
     widget->setGradientVisible(false);
 

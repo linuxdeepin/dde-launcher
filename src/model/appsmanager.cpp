f1f39cf15c2c489aea67523437b686182c1b6c68
weizhixiang <weizhixiang@uniontech.com>
2021-04-06 14:25:48 +0800

    fix: 修改应用程序desktop中图片路径后及时更新
    修改应用程序desktop中图片路径后，及时更新启动器中路径信息
    
    Log: 修改应用程序desktop中图片路径后及时更新
    Bug: https://pms.uniontech.com/zentao/bug-view-57552.html
    Change-Id: Icbe0c4815e72c0dba6a2db07bf9de446fa9a9860

--- src/model/appsmanager.cpp
+++ src/model/appsmanager.cpp
@@ -1087,6 +1087,14 @@ void AppsManager::handleItemChanged(const QString &operation, const ItemInfo &ap
         for (auto &item : m_allAppInfoList) {
             if (item == appInfo) {
                 item.updateInfo(appInfo);
+
+                for (QList<ItemInfo>::iterator it = m_userSortedList.begin(); it != m_userSortedList.end();) {
+                    if (*it == appInfo)
+                        it->updateInfo(appInfo);
+
+                    it++;
+                }
+
                 break;
             }
         }

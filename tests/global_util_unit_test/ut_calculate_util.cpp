fe29ad54622cfdd314fd1a39a1f8e6e151fddf3f
WenTao Song <songwentao@uniontech.com>
2021-04-16 11:36:20 +0800

    test: 优化启动器单元测试代码
    优化启动器单元测试,此处测试用例路径不应该使用固定路径做断言判断
    
    Log: 启动器单元测试优化
    Change-Id: I71cc3c3efa834971e4b291677e09d2e13ab052eb

--- tests/global_util_unit_test/ut_calculate_util.cpp
+++ tests/global_util_unit_test/ut_calculate_util.cpp
@@ -18,9 +18,39 @@ TEST_F(Tst_calculate, calendarSelectIcon_test)
     CalculateUtil::instance()->getScreenSize();
 
     QStringList iconList = CalculateUtil::instance()->calendarSelectIcon();
-    QVERIFY2(iconList.size() == 4, "iconList size isn't equal to 4...");
-    QVERIFY(iconList[0] == ":/icons/skin/icons/calendar_bg.svg");
-    QVERIFY(iconList[1] == ":/icons/skin/icons/calendar_month/month3.svg");
-    QVERIFY(iconList[2] == ":/icons/skin/icons/calendar_day/day4.svg");
-    QVERIFY(iconList[3] == ":/icons/skin/icons/calendar_week/week4.svg");
+    QVERIFY2((iconList.size() == 4), "iconList size isn't equal to 4...");
+    QVERIFY2(iconList.at(0).compare(":/icons/skin/icons/calendar_bg.svg"), "calendar background icon is abnormal...");
+
+    int month_num = QDate::currentDate().month();
+    switch(month_num)
+    {
+    case 1 ... 12:
+        QVERIFY2(iconList.at(1).compare(QString(":/icons/skin/icons/calendar_month/month%1.svg").arg(month_num)), "month num is abnormal...");
+        break;
+    default:
+        QVERIFY2(iconList.at(1).compare(QString(":/icons/skin/icons/calendar_month/month4.svg")), "default month num is abnormal...");
+        break;
+    }
+
+    int day_num = QDate::currentDate().day();
+    switch(day_num)
+    {
+    case 1 ... 31:
+        QVERIFY2(iconList.at(2).compare(QString(":/icons/skin/icons/calendar_day/day%1.svg").arg(day_num)), "day num is abnormal...");
+        break;
+    default:
+        QVERIFY2(iconList.at(2).compare(QString(":/icons/skin/icons/calendar_day/day23.svg")), "default day num is abnormal...");
+        break;
+    }
+
+    int week_num = QDate::currentDate().dayOfWeek();
+    switch(week_num)
+    {
+    case 1 ... 7:
+        QVERIFY2(iconList.at(3).compare(QString(":/icons/skin/icons/calendar_week/week%1.svg").arg(week_num)), "week num is abnormal...");
+        break;
+    default:
+        QVERIFY2(iconList.at(3).compare(QString(":/icons/skin/icons/calendar_week/week4.svg")), "defualt week num is abnormal...");
+        break;
+    }
 }

6fab93fc194cb68d1c5c09916397bd02bbcbdd4e
songwentao <songwentao@uniontech.com>
2021-08-25 15:27:26 +0800

    chore: 修改单元测试脚本
    从单元测试率统计脚本中过滤掉tests, dbusinterface, dbusservices目录,不统计单元测试本身代码和dbus接口相关的内容
    
    Log:
    Change-Id: Icc750acac043c4f73397ae83a90d5cde185e9844

--- gen_report.sh
+++ gen_report.sh
@@ -10,7 +10,9 @@ lcov -c -i -d ./ -o init.info
 ./dde_launcher_unit_test
 lcov -c -d ./ -o cover.info
 lcov -a init.info -a cover.info -o total.info
-lcov --remove total.info '*/usr/include/*' '*/usr/lib/*' '*/usr/lib64/*' '*/usr/local/include/*' '*/usr/local/lib/*' '*/usr/local/lib64/*' '*/third/*' 'testa.cpp' '*dbusinterface*' -o final.info
+
+#文件过滤,不统计以下目录覆盖率信息
+lcov --remove total.info '*/usr/include/*' '*/usr/lib/*' '*/usr/lib64/*' '*/usr/local/include/*' '*/usr/local/lib/*' '*/usr/local/lib64/*' '*/third/*' 'testa.cpp' '*dbusinterface*' '*tests*' '*dbusservices*' -o final.info
 
 # 生成报告
 genhtml -o cover_report --legend --title "lcov"  --prefix=./ final.info

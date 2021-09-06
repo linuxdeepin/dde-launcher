d364efa1a13391765bb721d3abc5a6979f698f00
songwentao <songwentao@uniontech.com>
2021-09-06 11:40:02 +0800

    test: 小窗口类单元测试添加
    WindowedFrame小窗口类添加单元测试，并处理段错误问题
    
    Log:
    Change-Id: I97cfb264da84ae69cc7b799807c3ee81cbf46f46

--- tests/ut_windowedframe.cpp
+++ tests/ut_windowedframe.cpp
@@ -7,116 +7,125 @@
 #include <gtest/gtest.h>
 
 class Tst_Windowedframe : public testing::Test
-{};
+{
+public:
+    void SetUp() override
+    {
+        m_windowedframe = new WindowedFrame(nullptr);
+    }
+
+    void TearDown() override
+    {
+        if (m_windowedframe) {
+            delete m_windowedframe;
+            m_windowedframe = nullptr;
+        }
+    }
+
+public:
+    WindowedFrame *m_windowedframe;
+};
 
 TEST_F(Tst_Windowedframe, privateMethod_test)
 {
-    WindowedFrame frame;
-
-    frame.moveCurrentSelectApp(Qt::Key_Undo);
+    m_windowedframe->moveCurrentSelectApp(Qt::Key_Undo);
     // 循环切换
     for (int count = 0; count < 6; count++) {
-        frame.moveCurrentSelectApp(Qt::Key_Tab);
+        m_windowedframe->moveCurrentSelectApp(Qt::Key_Tab);
     }
     // 循环回退
     for (int count = 0; count < 6; count++) {
-        frame.moveCurrentSelectApp(Qt::Key_Backtab);
+        m_windowedframe->moveCurrentSelectApp(Qt::Key_Backtab);
     }
-    frame.moveCurrentSelectApp(Qt::Key_Up);
-    frame.moveCurrentSelectApp(Qt::Key_Down);
-    frame.moveCurrentSelectApp(Qt::Key_Left);
-    frame.moveCurrentSelectApp(Qt::Key_Right);
+    m_windowedframe->moveCurrentSelectApp(Qt::Key_Up);
+    m_windowedframe->moveCurrentSelectApp(Qt::Key_Down);
+    m_windowedframe->moveCurrentSelectApp(Qt::Key_Left);
+    m_windowedframe->moveCurrentSelectApp(Qt::Key_Right);
 
     const QModelIndex index;
-    frame.switchToCategory(index);
+    m_windowedframe->switchToCategory(index);
 
-    frame.getCornerPath(WindowedFrame::AnchoredCornor::TopLeft);
-    frame.getCornerPath(WindowedFrame::AnchoredCornor::TopRight);
-    frame.getCornerPath(WindowedFrame::AnchoredCornor::BottomLeft);
-    frame.getCornerPath(WindowedFrame::AnchoredCornor::BottomRight);
+    m_windowedframe->getCornerPath(WindowedFrame::AnchoredCornor::TopLeft);
+    m_windowedframe->getCornerPath(WindowedFrame::AnchoredCornor::TopRight);
+    m_windowedframe->getCornerPath(WindowedFrame::AnchoredCornor::BottomLeft);
+    m_windowedframe->getCornerPath(WindowedFrame::AnchoredCornor::BottomRight);
 
-    frame.resetWidgetStyle();
-    frame.hideLauncher();
+    m_windowedframe->resetWidgetStyle();
+    m_windowedframe->hideLauncher();
 }
 
 TEST_F(Tst_Windowedframe, privateSlots_test)
 {
-    WindowedFrame frame;
-
-    frame.adjustPosition();
-    frame.onToggleFullScreen();
+    m_windowedframe->adjustPosition();
+    m_windowedframe->onToggleFullScreen();
 
     const double value = 0.5;
-    frame.onOpacityChanged(value);
+    m_windowedframe->onOpacityChanged(value);
     // display model switch
-    frame.onSwitchBtnClicked();
-    frame.onSwitchBtnClicked();
+    m_windowedframe->onSwitchBtnClicked();
+    m_windowedframe->onSwitchBtnClicked();
 
-    frame.onWMCompositeChanged();
+    m_windowedframe->onWMCompositeChanged();
 
-    frame.searchText("");
-    frame.searchText("test");
+    m_windowedframe->searchText("");
+    m_windowedframe->searchText("test");
 
-    frame.showTips("test");
-    frame.prepareHideLauncher();
-    frame.recoveryAll();
+    m_windowedframe->showTips("test");
+    m_windowedframe->prepareHideLauncher();
+    m_windowedframe->recoveryAll();
 }
 
 TEST_F(Tst_Windowedframe, eventFilter_test)
 {
-    WindowedFrame frame;
-
     QEvent event(QEvent::Type::None);
-    frame.m_eventFilter->eventFilter(nullptr, &event);
+    m_windowedframe->m_eventFilter->eventFilter(nullptr, &event);
 
-    QEvent event1(QEvent::Type::KeyPress);
-    QKeyEvent *keyPress = static_cast<QKeyEvent *>(&event1);
+    QEvent keyEvent(QEvent::Type::KeyPress);
+    QKeyEvent *keyPress = static_cast<QKeyEvent *>(&keyEvent);
 
     QKeyEvent f1KeyEvent(QEvent::Type::KeyPress, Qt::Key_F1, static_cast<QKeyEvent *>(keyPress)->modifiers());
-    frame.m_eventFilter->eventFilter(nullptr, &f1KeyEvent);
+    m_windowedframe->m_eventFilter->eventFilter(nullptr, &f1KeyEvent);
 
     QKeyEvent returnKeyEvent(QEvent::Type::KeyPress, Qt::Key_Return, static_cast<QKeyEvent *>(keyPress)->modifiers());
-    frame.m_eventFilter->eventFilter(nullptr, &returnKeyEvent);
+    m_windowedframe->m_eventFilter->eventFilter(nullptr, &returnKeyEvent);
 
     QKeyEvent escapeKeyEvent(QEvent::Type::KeyPress, Qt::Key_Escape, static_cast<QKeyEvent *>(keyPress)->modifiers());
-    frame.m_eventFilter->eventFilter(nullptr, &escapeKeyEvent);
+    m_windowedframe->m_eventFilter->eventFilter(nullptr, &escapeKeyEvent);
 
     QKeyEvent spaceKeyEvent(QEvent::Type::KeyPress, Qt::Key_Space, static_cast<QKeyEvent *>(keyPress)->modifiers());
-    frame.m_eventFilter->eventFilter(nullptr, &spaceKeyEvent);
+    m_windowedframe->m_eventFilter->eventFilter(nullptr, &spaceKeyEvent);
 
     QKeyEvent tabKeyEvent(QEvent::Type::KeyPress, Qt::Key_Tab, static_cast<QKeyEvent *>(keyPress)->modifiers());
-    frame.m_eventFilter->eventFilter(nullptr, &tabKeyEvent);
+    m_windowedframe->m_eventFilter->eventFilter(nullptr, &tabKeyEvent);
 
     QKeyEvent backSpaceKeyEvent(QEvent::Type::KeyPress, Qt::Key_Backspace, static_cast<QKeyEvent *>(keyPress)->modifiers());
-    frame.m_eventFilter->eventFilter(nullptr, &backSpaceKeyEvent);
+    m_windowedframe->m_eventFilter->eventFilter(nullptr, &backSpaceKeyEvent);
 }
 
 TEST_F(Tst_Windowedframe, updatePostion_test)
 {
-//    WindowedFrame frame;
+    int defaultPos = CalculateUtil::instance()->m_dockInter->position();
+    DGuiApplicationHelper::ColorType defaultMode = DGuiApplicationHelper::instance()->themeType();
 
-//    int defaultPos = CalculateUtil::instance()->m_dockInter->position();
-//    DGuiApplicationHelper::ColorType defaultMode = DGuiApplicationHelper::instance()->themeType();
+    /*
+     * 上: 0, 右: 1, 下: 2, 左: 3
+    */
+    for (int i = 0; i < 4; i++) {
+        m_windowedframe->m_dockInter->setPosition(i);
 
-//    /*
-//     * 上: 0, 右: 1, 下: 2, 左: 3
-//    */
-//    for (int i = 0; i < 4; i++) {
-//        frame.m_dockInter->setPosition(i);
+        /*
+         * 时尚模式: 0
+         * 高效模式: 1
+        */
+        for (int j = 0; j < 2; j++) {
+            m_windowedframe->m_dockInter->setDisplayMode(j);
+            m_windowedframe->updatePosition();
+            QTest::qWait(500);
+        }
+    }
 
-//        /*
-//         * 时尚模式: 0
-//         * 高效模式: 1
-//        */
-//        for (int j = 0; j < 2; j++) {
-//            frame.m_dockInter->setDisplayMode(j);
-//            frame.updatePosition();
-//            QTest::qWait(500);
-//        }
-//    }
-
-//    QTest::qWait(500);
-//    //　恢复默认位置．
-//    CalculateUtil::instance()->m_dockInter->setPosition(defaultPos);
-//    DGuiApplicationHelper::instance()->setThemeType(defaultMode);
+    QTest::qWait(500);
+    //　恢复默认位置．
+    CalculateUtil::instance()->m_dockInter->setPosition(defaultPos);
+    DGuiApplicationHelper::instance()->setThemeType(defaultMode);
 }

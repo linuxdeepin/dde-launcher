346246f552925748ac48809015cd9a0ca898cfd0
songwentao <songwentao@uniontech.com>
2021-09-08 16:23:13 +0800

    test: 给AppListView类添加单元测试
    AppListView添加单元测试
    
    Log:
    Change-Id: I744bc0bbedfa6f4d6210799ddd89191567da2be6

--- tests/view_unit_test/ut_applistview.cpp
+++ tests/view_unit_test/ut_applistview.cpp
@@ -1,149 +1,124 @@
-//#define private public
-//#include "appslistmodel.h"
-//#include "applistview.h"
-//#include "applistdelegate.h"
-//#include "windowedframe.h"
-//#undef private
+#define private public
+#include "appslistmodel.h"
+#include "applistview.h"
+#include "applistdelegate.h"
+#include "windowedframe.h"
+#undef private
 
-//#include <QTest>
-//#include <QApplication>
-//#include <QDropEvent>
-//#include <QDragEnterEvent>
-//#include <QMimeData>
-//#include <DGuiApplicationHelper>
+#include <QTest>
+#include <QApplication>
+#include <QDropEvent>
+#include <QDragEnterEvent>
+#include <QMimeData>
+#include <DGuiApplicationHelper>
 
-//#include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
-///**使用小窗口类WindowedFrame间接测试AppListView的
-// * 接口, 因为AppListView的视图代码是在小窗口类初始
-// * 化列表中创建的, 规避该类appListView_test中偶现的
-// * 崩溃问题
-// * @brief The Tst_Applistview class
-// */
-//class Tst_Applistview : public testing::Test
-//{};
+/**使用小窗口类WindowedFrame间接测试AppListView的
+ * 接口, 因为AppListView的视图代码是在小窗口类初始
+ * 化列表中创建的, 规避该类appListView_test中偶现的
+ * 崩溃问题
+ * @brief The Tst_Applistview class
+ */
+class Tst_Applistview : public testing::Test
+{
+public:
+    void SetUp() override
+    {
+        m_windowFrame = new WindowedFrame;
+    }
 
-//TEST_F(Tst_Applistview, appDelegate_test)
-//{
-//    WindowedFrame frame;
-//    AppListView *appListView = frame.m_appsView;
+    void TearDown() override
+    {
+        delete m_windowFrame;
+    }
 
-//    //　light type
-//    DGuiApplicationHelper::instance()->setThemeType(DGuiApplicationHelper::ColorType(1));
-//    appListView->setModel(frame.m_appsModel);
-//    appListView->setItemDelegate(new AppListDelegate(&frame));
-//    AppListDelegate * delegate = static_cast<AppListDelegate *>(appListView->itemDelegate());
-//    if (delegate)
-//        delegate->setActived(true);
+public:
+    WindowedFrame *m_windowFrame;
+};
 
-//    //　dark type
-//    DGuiApplicationHelper::instance()->setThemeType(DGuiApplicationHelper::ColorType(2));
-//    delegate = static_cast<AppListDelegate *>(appListView->itemDelegate());
-//    appListView->setItemDelegate(new AppListDelegate(frame.m_appsModel));
-//    if (delegate)
-//        delegate->setActived(true);
+TEST_F(Tst_Applistview, appDelegate_test)
+{
+    AppListView *appListView = m_windowFrame->m_appsView;
 
-//    frame.show();
-//    QTest::qWait(500);
-//    frame.hide();
-//    DGuiApplicationHelper::instance()->setThemeType(DGuiApplicationHelper::ColorType(1));
-//}
+    appListView->setModel(m_windowFrame->m_appsModel);
 
-//TEST_F(Tst_Applistview, event_test)
-//{
-//    AppListView widget;
-//    AppListDelegate delegate(&widget);
-//    widget.setItemDelegate(&delegate);
+    AppListDelegate delegate(appListView);
+    appListView->setItemDelegate(&delegate);
+    AppListDelegate * viewDelegate = static_cast<AppListDelegate *>(appListView->itemDelegate());
+    if (viewDelegate)
+        viewDelegate->setActived(true);
+}
 
-//    AppsListModel model(AppsListModel::All);
-//    QModelIndex index;
-//    model.insertRow(0, index);
-//    widget.setModel(&model);
+TEST_F(Tst_Applistview, event_test)
+{
+    AppListView *appListView = m_windowFrame->m_appsView;
 
-//    QTest::qWait(1000);
+    AppsListModel model(AppsListModel::All);
+    QModelIndex index;
+    model.insertRow(0, index);
+    appListView->setModel(&model);
 
-//    QWheelEvent wheelEvent(QPointF(0, 0), 0, Qt::MiddleButton, Qt::ControlModifier);
-//    widget.wheelEvent(&wheelEvent);
+    QWheelEvent wheelEvent(QPointF(0, 0), 0, Qt::MiddleButton, Qt::ControlModifier);
+    QApplication::sendEvent(appListView, &wheelEvent);
 
-//    QMouseEvent mouseMoveEvent(QEvent::MouseMove, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier, Qt::MouseEventSynthesizedByQt);
-//    widget.mouseMoveEvent(&mouseMoveEvent);
+    QMouseEvent mouseMoveEvent(QEvent::MouseMove, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier, Qt::MouseEventSynthesizedByQt);
+    QApplication::sendEvent(appListView, &mouseMoveEvent);
 
-//    QMouseEvent mousePress(QEvent::MouseButtonPress, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier, Qt::MouseEventSynthesizedByQt);
-//    widget.mousePressEvent(&mousePress);
+    QMouseEvent mousePress(QEvent::MouseButtonPress, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier, Qt::MouseEventSynthesizedByQt);
+    QApplication::sendEvent(appListView, &mousePress);
 
-//    widget.m_updateEnableSelectionByMouseTimer->start();
-//    widget.mousePressEvent(&mousePress);
+    appListView->hasAutoScroll();
+    QMouseEvent releaseEvent(QEvent::MouseButtonRelease, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier, Qt::MouseEventSynthesizedByQt);
+    QApplication::sendEvent(appListView, &releaseEvent);
 
-//    widget.hasAutoScroll();
-//    QMouseEvent releaseEvent(QEvent::MouseButtonRelease, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier, Qt::MouseEventSynthesizedByQt);
-//    widget.mouseReleaseEvent(&releaseEvent);
+    QMimeData mimeData;
+    mimeData.setData("test", "test");
 
-//    QMimeData mimeData;
-//    mimeData.setData("test", "test");
+    QDragEnterEvent dragEnterEvent(QPoint(0, 1), Qt::CopyAction, &mimeData, Qt::LeftButton, Qt::NoModifier);
+    QApplication::sendEvent(appListView, &dragEnterEvent);
 
-//    QDragEnterEvent dragEnterEvent(QPoint(0, 1), Qt::CopyAction, &mimeData, Qt::LeftButton, Qt::NoModifier);
-//    widget.dragEnterEvent(&dragEnterEvent);
+    QDragMoveEvent dragMoveEvent(QPoint(0, 2), Qt::MoveAction, &mimeData, Qt::LeftButton, Qt::NoModifier);
+    QApplication::sendEvent(appListView, &dragMoveEvent);
 
-//    QDragMoveEvent dragMoveEvent(QPoint(0, 2), Qt::MoveAction, &mimeData, Qt::LeftButton, Qt::NoModifier);
-//    widget.dragMoveEvent(&dragMoveEvent);
+    QDragLeaveEvent dragLeaveEvent;
+    QApplication::sendEvent(appListView, &dragLeaveEvent);
 
-//    QDragLeaveEvent dragLeaveEvent;
-//    widget.dragLeaveEvent(&dragLeaveEvent);
+    QDropEvent dropEvent(QPointF(0, 0), Qt::CopyAction, &mimeData, Qt::LeftButton, Qt::NoModifier);
+    QApplication::sendEvent(appListView, &dropEvent);
 
-//    QDropEvent dropEvent(QPointF(0, 0), Qt::CopyAction, &mimeData, Qt::LeftButton, Qt::NoModifier);
-//    widget.dropEvent(&dropEvent);
+    QEvent enterEvent(QEvent::Enter);
+    QApplication::sendEvent(appListView, &enterEvent);
 
-//    QEvent enterEvent(QEvent::Enter);
-//    widget.enterEvent(&enterEvent);
+    QEvent leaveEvent(QEvent::Leave);
+    QApplication::sendEvent(appListView, &leaveEvent);
 
-//    QEvent leaveEvent(QEvent::Leave);
-//    widget.leaveEvent(&leaveEvent);
+    appListView->handleScrollValueChanged();
+    appListView->handleScrollFinished();
+    appListView->prepareDropSwap();
+    appListView->dropSwap();
+    appListView->menuHide();
+}
 
-//    widget.handleScrollValueChanged();
-//    widget.handleScrollFinished();
-//    widget.prepareDropSwap();
-//    widget.dropSwap();
-//    widget.menuHide();
-//}
+TEST_F(Tst_Applistview, appListView_test)
+{
+    AppListView *appListView = m_windowFrame->m_appsView;
 
-//TEST_F(Tst_Applistview, appListView_test)
-//{
-//    WindowedFrame frame;
+    AppsListModel model(AppsListModel::All);
+    QModelIndex index;
+    model.insertRow(0, index);
+    appListView->setModel(&model);
+}
 
-//    AppListView *widget = frame.m_appsView;
-//    AppListDelegate delegate(&frame);
-//    widget->setItemDelegate(&delegate);
+TEST_F(Tst_Applistview, appListDelegate_test)
+{
+    AppListView *appListView = m_windowFrame->m_appsView;
+    AppListDelegate appListDelegate(appListView);
+    appListDelegate.setActived(false);
 
-//    AppsListModel model(AppsListModel::All);
-//    QModelIndex index;
-//    model.insertRow(0, index);
-//    widget->setModel(&model);
+    QPixmap pix;
+    appListDelegate.dropShadow(pix, 5, QColor(Qt::red), QPoint(10, 10));
 
-//    widget->handleScrollValueChanged();
-//    widget->handleScrollFinished();
-//    widget->prepareDropSwap();
-//    widget->dropSwap();
-//    widget->menuHide();
-//    frame.hide();
-//}
-
-//TEST_F(Tst_Applistview, appListDelegate_test)
-//{
-//    AppListDelegate appListDelegate;
-//    appListDelegate.setActived(false);
-
-//    ASSERT_FALSE(appListDelegate.m_actived);
-
-//    QPixmap pix;
-//    appListDelegate.dropShadow(pix, 5, QColor(Qt::red), QPoint(10, 10));
-
-//    pix.load("/usr/share/backgrounds/default_background.jpg");
-//    appListDelegate.dropShadow(pix, 5, QColor(Qt::red), QPoint(10, 10));
-//}
-
-//TEST_F(Tst_Applistview, viewApi_test)
-//{
-//    AppListView view;
-//    QModelIndex index = QModelIndex();
-//    view.startDrag(index);
-//}
+    pix.load("/usr/share/backgrounds/default_background.jpg");
+    appListDelegate.dropShadow(pix, 5, QColor(Qt::red), QPoint(10, 10));
+}

e6aabe647d3fbb69d41cfe817a1a116852cb3e09
songwentao <songwentao@uniontech.com>
2021-09-08 16:32:32 +0800

    test: 给MultiPagesView添加单元测试
    给MultiPagesView添加单元测试
    
    Log:
    Change-Id: I89e149821e24b604821afa81415b9f8bb5d7b156

--- tests/view_unit_test/ut_multipagesview.cpp
+++ tests/view_unit_test/ut_multipagesview.cpp
@@ -1,47 +1,59 @@
-//#define private public
-//#include "fullscreenframe.h"
-//#undef private
+#define private public
+#include "fullscreenframe.h"
+#undef private
 
-//#include <QApplication>
-//#include <QTest>
+#include <QApplication>
+#include <QTest>
 
-//#include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
-///** Multipagesview的某些接口调用依赖于FullScreenFrame类中的数据
-// * 因此使用FullScreenFrame来间接测试Multipagesview的接口
-// * @brief The Tst_Multipagesview class
-// */
-//class Tst_Multipagesview : public testing::Test
-//{};
+/** Multipagesview的某些接口调用依赖于FullScreenFrame类中的数据
+ * 因此使用FullScreenFrame来间接测试Multipagesview的接口
+ * @brief The Tst_Multipagesview class
+ */
+class Tst_Multipagesview : public testing::Test
+{
+public:
+    void SetUp() override
+    {
+        m_fullscreen = new FullScreenFrame;
+    }
 
-//TEST_F(Tst_Multipagesview, multiPagesView_test)
-//{
-//   该类使用当前方法调用很容易出现段错误，本地无法复现，包括tty,但gerrit会出现，暂时屏蔽，放到最后处理。
-//    CalculateUtil::instance()->setDisplayMode(0);
+    void TearDown() override
+    {
+        delete m_fullscreen;
+    }
 
-//    FullScreenFrame frame;
+public:
+    FullScreenFrame *m_fullscreen;
+};
 
-//    MultiPagesView view(AppsListModel::All, &frame);
+TEST_F(Tst_Multipagesview, multiPagesView_test)
+{
+    m_fullscreen->m_calcUtil->setDisplayMode(0);
+    MultiPagesView *multipageView = m_fullscreen->m_multiPagesView;
 
-//    view.setViewContentMargin();
+    QPixmap pix;
+    multipageView->updateGradient(pix, QPoint(0, 0), QPoint(10, 10));
+    multipageView->setViewContentMargin();
+    multipageView->updateGradient();
+    multipageView->setGradientVisible(false);
+}
 
-//    view.updateGradient();
+TEST_F(Tst_Multipagesview, event_test)
+{
+    MultiPagesView *multipageView = m_fullscreen->m_multiPagesView;
+    multipageView->updatePageCount(AppsListModel::Category);
 
-//    QPixmap pix;
-//    view.updateGradient(pix, QPoint(0, 0), QPoint(10, 10));
+    QMouseEvent mousePressEvent(QEvent::MouseButtonPress, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier);
+    QApplication::sendEvent(multipageView, &mousePressEvent);
 
-//    view.updatePageCount(AppsListModel::Category);
-//    QMouseEvent mousePressEvent(QEvent::MouseButtonPress, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier);
-//    view.mousePressEvent(&mousePressEvent);
+    multipageView->updatePageCount(AppsListModel::Internet);
+    QApplication::sendEvent(multipageView, &mousePressEvent);
 
-//    view.updatePageCount(AppsListModel::Internet);
-//    view.mousePressEvent(&mousePressEvent);
+    QMouseEvent mouseMoveEvent(QEvent::MouseMove, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier);
+    QApplication::sendEvent(multipageView, &mouseMoveEvent);
 
-//    QMouseEvent mouseMoveEvent(QEvent::MouseMove, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier);
-//    view.mouseMoveEvent(&mouseMoveEvent);
-
-//    QMouseEvent mouseReleaseEvent(QEvent::MouseButtonRelease, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier);
-//    view.mouseReleaseEvent(&mouseReleaseEvent);
-
-//    view.setGradientVisible(false);
-//}
+    QMouseEvent mouseReleaseEvent(QEvent::MouseButtonRelease, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier);
+    QApplication::sendEvent(multipageView, &mouseReleaseEvent);
+}

262c68853e2ef58ac71a2c51a08e18ad983e85ca
songwentao <songwentao@uniontech.com>
2021-09-09 17:23:04 +0800

    test: 给ModeToggleButton添加单元测试
    给ModeToggleButton添加单元测试
    
    Log:
    Change-Id: I630d92367fd9a5e0d4778fc75829581e4c282095

--- tests/widget_unit_test/ut_modetogglebutton.cpp
+++ tests/widget_unit_test/ut_modetogglebutton.cpp
@@ -16,29 +16,34 @@ class Tst_Modetogglebutton : public testing::Test
 
 TEST_F(Tst_Modetogglebutton, modeToggleButton_test)
 {
-//    ModeToggleButton button;
+    ModeToggleButton button;
 
-//    DGuiApplicationHelper::ColorType defaultType = DGuiApplicationHelper::instance()->themeType();
+    DGuiApplicationHelper::ColorType defaultType = DGuiApplicationHelper::instance()->themeType();
 
-//    button.setFocus();
-//    button.m_hover = true;
-//    QPaintEvent paintEvent1(QRect(10, 10, 10, 10));
-//    button.paintEvent(&paintEvent1);
+    for (int i = 1; i <= 2; i++) {
+        DGuiApplicationHelper::instance()->setThemeType(DGuiApplicationHelper::ColorType(i));
+        button.resetStyle();
+        QTest::qWait(50);
+    }
 
-//    QEvent enterEvent(QEvent::Enter);
-//    button.enterEvent(&enterEvent);
+    DGuiApplicationHelper::instance()->setThemeType(defaultType);
+}
 
-//    QEvent leaveEvent(QEvent::Leave);
-//    button.leaveEvent(&leaveEvent);
+TEST_F(Tst_Modetogglebutton, event_test)
+{
+    ModeToggleButton button;
 
-//    QMouseEvent releaseEvent(QEvent::MouseButtonRelease, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier, Qt::MouseEventSynthesizedByQt);
-//    button.mouseReleaseEvent(&releaseEvent);
+    button.setFocus();
+    button.m_hover = true;
+    QPaintEvent paintEvent(QRect(10, 10, 10, 10));
+    QApplication::sendEvent(&button, &paintEvent);
 
-//    for (int i = 0; i <= 2; i++) {
-//        DGuiApplicationHelper::instance()->setThemeType(DGuiApplicationHelper::ColorType(i));
-//        button.resetStyle();
-//        QTest::qWait(10);
-//    }
+    QEvent enterEvent(QEvent::Enter);
+    QApplication::sendEvent(&button, &enterEvent);
 
-//    DGuiApplicationHelper::instance()->setThemeType(defaultType);
+    QEvent leaveEvent(QEvent::Leave);
+    QApplication::sendEvent(&button, &leaveEvent);
+
+    QMouseEvent releaseEvent(QEvent::MouseButtonRelease, QPointF(0, 0), QPointF(0, 1), QPointF(1, 1), Qt::LeftButton, Qt::LeftButton, Qt::NoModifier, Qt::MouseEventSynthesizedByQt);
+    QApplication::sendEvent(&button, &releaseEvent);
 }

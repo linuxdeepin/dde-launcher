0e7c4231cb1923f574ed7c510684616d2a0293b0
songwentao <songwentao@uniontech.com>
2021-12-02 20:13:23 +0800

    test: 更新右键菜单的单元测试
    右键菜单
    
    Log: test
    Task: https://pms.uniontech.com/zentao/task-view-86284.html
    Influence: 启动器-单元测试
    Change-Id: I19430863de06bea33677e9b5960a5e62bd4edd49

--- tests/worker_unit_test/ut_menuworker.cpp
+++ tests/worker_unit_test/ut_menuworker.cpp
@@ -1,5 +1,6 @@
 #define private public
 #include "menuworker.h"
+#include "menudialog.h"
 #undef private
 
 #include <QTest>
@@ -13,18 +14,39 @@ class Tst_MenuWorker : public testing::Test
 TEST_F(Tst_MenuWorker, menuWorker_test)
 {
     MenuWorker worker;
-    QMenu menu;
-    menu.setAccessibleName("popmenu");
 
-    QSignalMapper *signalMapper = new QSignalMapper(&menu);
-    worker.creatMenuByAppItem(&menu, signalMapper);
+    QSignalMapper *signalMapper = new QSignalMapper(worker.m_menu);
+    worker.creatMenuByAppItem(worker.m_menu, signalMapper);
 
     for (int index = 0; index < 7; index++)
         signalMapper->mapped(1);
 
-    menu.aboutToHide();
+    worker.m_menu->aboutToHide();
 
     const QModelIndex index;
     worker.setCurrentModelIndex(index);
     QVERIFY(index == worker.getCurrentModelIndex());
+
+    worker.isMenuVisible();
+    worker.isMenuShown();
+    worker.onHideMenu();
+}
+
+TEST_F(Tst_MenuWorker, menu_action_test)
+{
+    Menu menu;
+    for (int i = 0; i < 5; i++) {
+        QAction *action = new QAction(&menu);
+        menu.addAction(action);
+    }
+
+    menu.setActiveAction(menu.actions().at(0));
+
+    for (int i = 0; i < menu.actions().size(); i++)
+        menu.moveUp(i);
+
+    for (int i = 0; i < menu.actions().size(); i++)
+        menu.moveDown(i);
+
+    menu.openItem();
 }

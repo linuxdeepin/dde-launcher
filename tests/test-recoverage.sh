d633d3e3c92650c762ea7d6efce605ea995f914a
沈文骐 <shenwenqi@uniontech.com>
2022-02-17 13:32:31 +0800

    feat: 单元测试结果目录结构调整
    修改单元测试结果目录结构
    
    Log: 单元测试结果目录结构调整
    Task: https://pms.uniontech.com/zentao/task-view-100126.html
    Influence: 单元测试输出结果
    Change-Id: Id0383d488ca9b5fefe518b621aa5e090d258a26c

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-BUILD_DIR=build
+BUILD_DIR=build-ut
+HTML_DIR=html
 REPORT_DIR=report
 
 cd ../
@@ -8,19 +9,23 @@ rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 
+mkdir -p $HTML_DIR
+mkdir -p $REPORT_DIR
+
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make -j 8
 
 cd tests/
 
 lcov -c -i -d ./ -o init.info
-./dde_launcher_unit_test --gtest_output=xml:dde_test_report_dde_launcher.xml
+./dde_launcher_unit_test --gtest_output=xml:../$REPORT_DIR/ut-report_dde_launcher.xml
 lcov -c -d ./ -o cover.info
 lcov -a init.info -a cover.info -o total.info
-lcov -r total.info "*/tests/*" "*/usr/include*" "*build/src*" '*dbusinterface*' '*tests*' '*dbusservices*' -o final.info
+lcov -r total.info "*/tests/*" "*/usr/include*" "*build/src*" '*dbusinterface*' '*tests*' '*dbusservices*' "*/dde-launcher_autogen/*" -o final.info
 
-rm -rf ../../tests/$REPORT_DIR
-mkdir -p ../../tests/$REPORT_DIR
-genhtml -o ../../tests/$REPORT_DIR final.info
 
-mv asan.log* asan_dde-launcher.log
+genhtml -o ../$HTML_DIR final.info
+
+mv ../$HTML_DIR/index.html ../$HTML_DIR/cov_dde-launcher.html
+
+mv asan.log* ../asan_dde-launcher.log

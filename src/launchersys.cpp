59c424f92080b9d7c21dbf5d1ac947c33bf4f334
Fan PengCheng <fanpengcheng@uniontech.com>
2022-03-11 22:08:24 +0800

    feat: 配置项规范化
    配置项规范化
    
    Log:
    Influence: 无
    Task: https://pms.uniontech.com/task-view-104239.html
    Change-Id: I508118ac744322f64a9755d3ab33488a67ef0c1f

--- src/launchersys.cpp
+++ src/launchersys.cpp
@@ -272,7 +272,7 @@ void LauncherSys::aboutToShowLauncher()
 
 void LauncherSys::preloadIcon()
 {
-    if (!getDConfigValue("preload-apps-icon", true).toBool())
+    if (!getDConfigValue("preloadAppsIcon", true).toBool())
         return;
 
     if (!m_dbusLauncherInter->fullscreen()) {

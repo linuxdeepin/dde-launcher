f5ad3530c35e8a753672e9882e668fa45f1acba6
殷杰 <yinjie@uniontech.com>
2022-03-15 16:07:26 +0800

    feat: 配置项规范化, 使用appId获取配置
    配置项规范化
    
    Log:
    Influence: 无
    Task: https://pms.uniontech.com/task-view-104239.html
    Change-Id: Iaba591bb4052e3cacb1ba341436cfbfc650e3d59
    (cherry picked from commit 29bdcf08b4c181f562d7ec89e0ba5c4c9d1cb00d)

--- src/global_util/util.cpp
+++ src/global_util/util.cpp
@@ -39,6 +39,7 @@
 #include <QIcon>
 #include <QScopedPointer>
 #include <QIconEngine>
+#include <QSharedPointer>
 
 #include <private/qguiapplication_p.h>
 #include <private/qiconloader_p.h>
@@ -489,16 +490,15 @@ QString cacheKey(const ItemInfo &itemInfo)
  */
 QVariant getDConfigValue(const QString &key, const QVariant &defaultValue, const QString &configFileName)
 {
-    DConfig config(configFileName);
-
-    if (!config.isValid()) {
+    QSharedPointer<DConfig> config(DConfig::create(DLauncher::DEFAULT_META_CONFIG_NAME, DLauncher::DEFAULT_META_CONFIG_NAME));
+    if (!config->isValid()) {
         qWarning() << QString("DConfig is invalid, name:[%1], subpath[%2].").
-                        arg(config.name(), config.subpath());
+                        arg(config->name(), config->subpath());
         return defaultValue;
     }
 
-    if (config.keyList().contains(key))
-        return config.value(key);
+    if (config->keyList().contains(key))
+        return config->value(key);
 
     return defaultValue;
 }

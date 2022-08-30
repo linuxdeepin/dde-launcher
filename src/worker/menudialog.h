cd3bb3fe7516b461df4a8e1f509e506dfac5032a
shaojun <wangshaojun_cm@deepin.com>
2022-08-30 16:56:58 +0800

    chore: adapt license and copyright
    Modify project to pass the REUSE check
    
    Log: Modify project to pass the REUSE check
    Task: https://pms.uniontech.com/task-view-185215.html

--- src/worker/menudialog.h
+++ src/worker/menudialog.h
@@ -1,27 +1,6 @@
-/*
- * Copyright (C) 2020 ~ 2021 Deepin Technology Co., Ltd.
- *
- * Author:     songwentao <songwentao@uniontech.com>
- *
- * Maintainer: songwentao <songwentao@uniontech.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * This program aims to cache the the icon and name of apps to the hash table,
- * which can decrease the repeated resource consumption of loading the app info in the
- * running time.
- */
+// SPDX-FileCopyrightText: 2020 - 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #ifndef MYMENU_H
 #define MYMENU_H
